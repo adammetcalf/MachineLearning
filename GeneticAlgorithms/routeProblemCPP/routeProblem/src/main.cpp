70d901344006a15ec559897be5948d910d0b1952
Adam Metcalf <mn14agm@leeds.ac.uk>
2021-07-30 13:12:56 +0100

    tried plplot lib. can create a graph but hard to know how to do a scatter plot, documentation is poor

--- GeneticAlgorithms/routeProblemCPP/routeProblem/src/main.cpp
+++ GeneticAlgorithms/routeProblemCPP/routeProblem/src/main.cpp
@@ -5,6 +5,9 @@
 #include "classes/Individual.h"
 #include "classes/Population.h"
 
+#include <cstring>
+#include <cmath>
+#include "plplot\plstream.h"
 
 int main()
 {
@@ -15,7 +18,7 @@ int main()
 	
 
     std::vector < std::vector<float> > World = generateWorld(worldSize);                        //generate the world
-    //printVector(World);                                                                         //print the vectors
+    //printVector(World);                                                                        //print the vectors
 	
 	/* Testing the individual class working properly
 	
@@ -29,6 +32,24 @@ int main()
 		std::cout << element << " " << '\n';
 	}
 	*/
+
+	//plot using plplot
+
+	PLFLT x[50], y[50];
+	PLFLT xmin = 0., xmax = 1., ymin = 0., ymax = 100.;
+	int   i;
+	for (i = 0; i < 50; i++) {
+		x[i] = (PLFLT)(i) / (PLFLT)(50 - 1);
+		y[i] = ymax * x[i] * x[i];
+	}
+	auto pls = new plstream();
+	plsdev("wingcc");
+	pls->init();
+	pls->env(xmin, xmax, ymin, ymax, 0, 0);
+	pls->lab("x", "y=100 x#u2#d", "Simple PLplot demo of a 2D line plot");
+	pls->line(50, x, y);
+	delete pls;
+
 
 	Population popCheck(World, worldSize);														//create a population
 
